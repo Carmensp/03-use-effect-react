3f90cb1c2424914ad02de38cc5f5e5c682ae9b1b
carmensp <carmen.8.2.0.3.sp@gmail.com>
2025-01-16 13:14:42 +0100

    primer commit

---
+++ src/components/PruebasComponent.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react'
+
+export const PruebasComponent = () => {
+
+    const [usuario, setUsuario] = useState("Carmen");
+    const [fecha, setFecha] = useState("01-02-2014");
+    //const [contador, setContador] = useState(0);
+
+    const modUsuario = e => {
+        setUsuario(e.target.value);
+    }
+    const modFecha = e => {
+        setFecha(Date.now());
+    }
+
+    useEffect(() => {
+        console.log("Has cargado el componente PruebasComponent o has realizado un cambio en un estado");
+    })
+
+    useEffect(() => {
+        console.log("Has cargado el componente PruebasComponent");
+    }, [])
+
+    useEffect(() => {
+        //setContador(contador+1);
+        console.log("Has modificado el usuario");
+    }, [usuario])
+
+  return (
+    <div>
+        <h1>El Efecto - Hook UseEffect</h1>
+        <strong className='label label-green'>{usuario}</strong>
+        <strong>{fecha}</strong>
+        <form>
+            <input type='text' onChange={modUsuario} placeholder='Cambia el nombre'/>
+            <button onClick={modFecha}>CAMBIAR FECHA</button>
+        </form>
+    </div>
+  )
+}
